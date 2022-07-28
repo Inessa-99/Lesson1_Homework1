97cac2206d81d20689e74f576afbd377cf2934be
Inessa <inesa-99@mail.ru>
2022-07-28 20:05:44 +0300

    Добавили папки с ДЗ

---
+++ HW1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт максимальное из этих чисел
+Console.WriteLine("Введите первое число" );
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число" );
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Resoult: " + a);
+}
+else
+{
+    Console.WriteLine("Resoult: " + b);
+}
---
+++ HW4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число" );
+int a = int.Parse(Console.ReadLine());
+int i = 2;
+
+while (i <= a) {
+    if (a % 2 == 0)
+    {
+        
+        Console.Write(i + " ");
+        i = i+2;
+    }
+    else
+    {
+        i = i+2;
+        Console.Write(i + " ");
+    }
+}
